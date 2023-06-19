652e4c6f70bb5346c1cd51bef73a21696e390ffe
icedwolf <celio.junior@bludata.com.br>
2023-06-19 08:28:57 -0300

    att

--- config.lua
+++ config.lua
@@ -5,91 +5,91 @@ vim.opt.wrap = true
 lvim.colorscheme = "lunar"
 lvim.log.level = "info"
 lvim.format_on_save = {
-    enabled = true,
-    pattern = "*.lua",
-    timeout = 1000,
+  enabled = true,
+  pattern = "*.lua",
+  timeout = 1000,
 }
 lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.builtin.treesitter.ensure_installed = {
-    "bash",
-    "c",
-    "javascript",
-    "json",
-    "lua",
-    "python",
-    "typescript",
-    "tsx",
-    "css",
-    "rust",
-    "java",
-    "yaml",
-    "php"
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+  "php"
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
 lvim.plugins = {
-    {
-        'wfxr/minimap.vim',
-        cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
-        config = function()
-            vim.cmd("let g:minimap_width = 10")
-            vim.cmd("let g:minimap_auto_start = 1")
-            vim.cmd("let g:minimap_auto_start_win_enter = 1")
-        end,
+  {
+    'wfxr/minimap.vim',
+    cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
+    config = function()
+      vim.cmd("let g:minimap_width = 10")
+      vim.cmd("let g:minimap_auto_start = 1")
+      vim.cmd("let g:minimap_auto_start_win_enter = 1")
+    end,
+  },
+  {
+    "sindrets/diffview.nvim",
+    event = "BufRead",
+  },
+  {
+    "tpope/vim-fugitive",
+    cmd = {
+      "G",
+      "Git",
+      "Gdiffsplit",
+      "Gread",
+      "Gwrite",
+      "Ggrep",
+      "GMove",
+      "GDelete",
+      "GBrowse",
+      "GRemove",
+      "GRename",
+      "Glgrep",
+      "Gedit"
     },
-    {
-        "sindrets/diffview.nvim",
-        event = "BufRead",
-    },
-    {
-        "tpope/vim-fugitive",
-        cmd = {
-            "G",
-            "Git",
-            "Gdiffsplit",
-            "Gread",
-            "Gwrite",
-            "Ggrep",
-            "GMove",
-            "GDelete",
-            "GBrowse",
-            "GRemove",
-            "GRename",
-            "Glgrep",
-            "Gedit"
-        },
-        ft = { "fugitive" }
-    },
-    {
-        "folke/trouble.nvim",
-        cmd = "TroubleToggle",
-    },
-    {
-        "npxbr/glow.nvim",
-        ft = { "markdown" }
-    },
-    { "tpope/vim-repeat" },
-    {
-        "tpope/vim-surround",
-    },
-    {
-        "ThePrimeagen/refactoring.nvim",
-    }
+    ft = { "fugitive" }
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
+  {
+    "npxbr/glow.nvim",
+    ft = { "markdown" }
+  },
+  { "tpope/vim-repeat" },
+  {
+    "tpope/vim-surround",
+  },
+  {
+    "ThePrimeagen/refactoring.nvim",
+  }
 }
 
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-    { command = "eslint", filetypes = { "typescript", "typescriptreact" } }
+  { command = "eslint", filetypes = { "typescript", "typescriptreact" } }
 }
 
 
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-    {
-        command = "prettier",
-        filetypes = { "typescript", "typescriptreact" },
-    },
+  {
+    command = "prettier",
+    filetypes = { "typescript", "typescriptreact" },
+  },
 }
