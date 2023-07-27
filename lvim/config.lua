5935117100704d99767bed113182362fb8479661
icedwolf <romaxino@gmail.com>
2023-07-27 08:39:46 -0300

    conflicting plugins on nvim

--- lvim/config.lua
+++ lvim/config.lua
@@ -142,14 +142,14 @@ lvim.plugins = {
             })
         end,
     },
-    {
-        "github/copilot.vim",
-        event = "VeryLazy",
-        config = function()
-            vim.g.copilot_assume_mapped = true
-            vim.g.copilot_no_tab_map = true
-        end
-    },
+    -- {
+    --     "github/copilot.vim",
+    --     event = "VeryLazy",
+    --     config = function()
+    --         vim.g.copilot_assume_mapped = true
+    --         vim.g.copilot_no_tab_map = true
+    --     end
+    -- },
     {
         "norcalli/nvim-colorizer.lua",
         config = function()
