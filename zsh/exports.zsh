4a99ea79eff25235655a104755928c5845d8c2ce
icedwolf <romaxino@gmail.com>
2023-08-28 10:54:29 -0300

    att

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -4,3 +4,4 @@ export KB=$HOME/.kube
 export KUBECONFIG=$KB/credinet_prod:$KB/dev:$KB/local:$KB/cred-hml:$KB/rancher01:$KB/rancher01-hml:$KB/rancher-ggdc:$KB/k3d
 export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/podman/podman.sock
 export DOCKER_SOCK=$XDG_RUNTIME_DIR/podman/podman.sock
+export MINIKUBE_HOME=$XDG_CONFIG_HOME
