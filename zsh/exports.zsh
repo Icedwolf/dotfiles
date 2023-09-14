084cb0add90565c6e25b83a1f58e86a089c38f84
icedwolf <romaxino@gmail.com>
2023-09-14 15:39:54 -0300

    att

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,7 +1,7 @@
 export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 export FZF_DEFAULT_COMMAND='rg --files --hidden --glob "!.git"'
 export KB=$HOME/.kube
-export KUBECONFIG=$KB/credinet_prod:$KB/cred-hml:$KB/rancher01:$KB/rancher01-hml:$KB/rancher-ggdc:$KB/k3d
+export KUBECONFIG=$KB/credinet_prod:$KB/cred-hml:$KB/rancher01:$KB/rancher01-hml:$KB/rancher-ggdc
 export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/podman/podman.sock
 export DOCKER_SOCK=$XDG_RUNTIME_DIR/podman/podman.sock
 export MINIKUBE_HOME=$XDG_CONFIG_HOME
