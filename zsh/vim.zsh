2ce7211b07089efd8797bb2c37c07d895853814e
icedwolf <romaxino@gmail.com>
2024-04-03 12:53:29 -0300

    fix zsh vim keybinds

--- zsh/vim.zsh
+++ zsh/vim.zsh
@@ -1,8 +1,8 @@
 bindkey -v
 bindkey "^k" atuin-up-search
-bindkey "^j" atuin-down-search
+bindkey "^j" down-line-or-search
 bindkey -M vicmd "^k" atuin-up-search
-bindkey -M vicmd "^j" atuin-down-search
+bindkey -M vicmd "^j" down-line-or-search
 bindkey '^w' backward-kill-word
 bindkey '^r' history-incremental-search-backward
 bindkey '^n' autosuggest-accept
