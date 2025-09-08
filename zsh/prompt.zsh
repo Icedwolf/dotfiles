ddb18bef92e350aac7171748a46114ac82396c6e
icedwolf <romaxino@gmail.com>
2025-09-08 09:29:12 -0300

    updating prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,11 +1,17 @@
-autoload -Uz vcs_info colors && colors
+autoload -Uz vcs_info
 setopt PROMPT_SUBST
-precmd() {
-  vcs_info
-}
+
 zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:git:*' formats "%{${fg[yellow]}%} on %b%{${fg[red]}%}%u%{${fg[green]}%}%s%{${reset_color}%}"
-zstyle ':vcs_info:git:*' stagedstr ' ● '
-zstyle ':vcs_info:git:*' unstagedstr ' ✚ '
-PROMPT=$'%{${fg[cyan]}%}%d%{${reset_color}%}${vcs_info_msg_0_}\n%{${fg[green]}%}❯%{${reset_color}%} '
+zstyle ':vcs_info:*' check-for-staged-changes true
+zstyle ':vcs_info:*' check-for-untracked true
+zstyle ':vcs_info:git:*' formats "on %F{yellow}%b%f%F{red}%u%F{green}%c%f"
+zstyle ':vcs_info:git:*' actionformats "on %F{yellow}%b%f %F{red}| %a%f"
+zstyle ':vcs_info:git:*' stagedstr ' ●'
+zstyle ':vcs_info:git:*' unstagedstr ' ✚'
+
+precmd() {
+    vcs_info
+}
+
+PROMPT=$'%(!.%F{red}%n%f.%F{green}%n%f)@%F{blue}%m%f %F{cyan}%d%f ${vcs_info_msg_0_} \n%F{green}❯%f '
