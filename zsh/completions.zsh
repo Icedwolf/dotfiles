88a95889df51c93c7206f23b72e245ea1258be63
icedwolf <romaxino@gmail.com>
2026-02-03 18:42:12 -0300

    youtui config

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -48,3 +48,5 @@ zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f
 ## For kubernetes
 zstyle ':completion:*:*:kubectl:*' list-grouped false
 compdef _get_kube_contexts ucontext
+
+compdef _zshz ${ZSHZ_CMD:-z}
